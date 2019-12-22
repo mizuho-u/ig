b7ace9047e20b5cc3762e215a8b78d030ee046cc
Mizuho Mac <mizu.2340@gmail.com>
2019-12-22 23:40:55 +0900

    typo

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -9,14 +9,14 @@ import (
 func TestNextToken(t *testing.T) {
 
 	input := `let five = 5;
-	let ten = 10;
+let ten = 10;
 
-	let add = fn(x, y) {
-		x + yl
-	};
+let add = fn(x, y) {
+		x + y;
+};
 
-	let result = add(five, ten);
-	`
+let result = add(five, ten);
+`
 
 	tests := []struct {
 		expectedType    token.TokenType
@@ -71,7 +71,7 @@ func TestNextToken(t *testing.T) {
 		tok := l.NextToken()
 
 		if tok.Type != tt.expectedType {
-			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Literal)
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
 		}
 
 		if tok.Literal != tt.expectedLiteral {
