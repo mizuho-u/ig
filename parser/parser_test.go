bcfa7fb491c0544cc17e379a50729339e4352323
Mizuho Mac <mizu.2340@gmail.com>
2020-01-18 23:54:46 +0900

    2-6-6
    parseFns

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -112,3 +112,26 @@ return 838383;
 	}
 
 }
+
+func TestIdentifierExpression(t *testing.T) {
+	input := "foobar;"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program has not enough statements. got=%d", len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T", program.Statements[0])
+	}
+
+	ident, ok := stmt.Expression.(*ast.Identifier)
+	if !ok {
+		t.Errorf("ident.Value not %s. got=%s", "foobar", ident.TokenLiteral())
+	}
+}
