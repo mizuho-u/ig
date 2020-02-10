cc32a1271c8f7ecedaedd530941154a36f8379de
Mizuho Mac <mizu.2340@gmail.com>
2020-02-11 02:07:01 +0900

    2-9
    perser epel

--- repl/repl.go
+++ repl/repl.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"io"
 	"lexer"
-	"token"
+	"parser"
 )
 
 // PROMPT ...
@@ -24,9 +24,39 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParseErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+// MonkeyFace ...
+const MonkeyFace = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
+
+func printParseErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MonkeyFace)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
