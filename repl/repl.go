b10e0ac1d33b8bcec71f7435f23421ac83924401
Mizuho Mac <mizu.2340@gmail.com>
2020-02-22 19:21:28 +0900

    3-9 eval identifier env get, set

--- repl/repl.go
+++ repl/repl.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"lexer"
+	"object"
 	"parser"
 )
 
@@ -15,6 +16,7 @@ const PROMPT = ">> "
 // Start ...
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Print(PROMPT)
@@ -33,7 +35,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
