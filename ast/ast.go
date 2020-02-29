adba83bc3315d827e1d2148b1973f8b8318a8765
Mizuho Mac <mizu.2340@gmail.com>
2020-02-29 23:08:36 +0900

    4-5-2 parse hash literal

--- ast/ast.go
+++ ast/ast.go
@@ -369,3 +369,29 @@ func (ie *IndexExpression) String() string {
 
 	return out.String()
 }
+
+// HashLiteral ...
+type HashLiteral struct {
+	Token token.Token // token {
+	Pairs map[Expression]Expression
+}
+
+func (hl *HashLiteral) expressionNode() {}
+
+// TokenLiteral ...
+func (hl *HashLiteral) TokenLiteral() string { return hl.Token.Literal }
+
+func (hl *HashLiteral) String() string {
+	var out bytes.Buffer
+
+	pairs := []string{}
+	for key, value := range hl.Pairs {
+		pairs = append(pairs, key.String()+":"+value.String())
+	}
+
+	out.WriteString("{")
+	out.WriteString(strings.Join(pairs, ", "))
+	out.WriteString("}")
+
+	return out.String()
+}
