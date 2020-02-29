f6e53f7e7c2328f1d7fa79be56d4549dcaaa0475
Mizuho Mac <mizu.2340@gmail.com>
2020-02-29 22:33:14 +0900

    4-5-1 lexer hash

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -30,6 +30,7 @@ if (5 < 10) {
 "foobar"
 "foo bar"
 [1, 2];
+{"foo": "bar"}
 `
 
 	tests := []struct {
@@ -126,6 +127,13 @@ if (5 < 10) {
 		{token.INT, "2"},
 		{token.RBRACKET, "]"},
 		{token.SEMICOLON, ";"},
+
+		{token.LBRACE, "{"},
+		{token.STRING, "foo"},
+		{token.COLON, ":"},
+		{token.STRING, "bar"},
+		{token.RBRACE, "}"},
+
 		{token.EOF, ""},
 	}
 
