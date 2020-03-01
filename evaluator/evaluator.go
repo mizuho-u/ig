c3f02ace7908b2ca5fea24916d260054b3c3f756
Mizuho Mac <mizu.2340@gmail.com>
2020-03-01 21:37:19 +0900

    4-5-4 eval HashLiteral

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -106,6 +106,9 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		}
 
 		return evalIndexExpression(left, index)
+	case *ast.HashLiteral:
+
+		return evalHashLiteral(node, env)
 	}
 
 	return nil
@@ -338,7 +341,6 @@ func evalStringInfixExpression(operator string, left, right object.Object) objec
 	case "+":
 		return &object.String{Value: leftVal + rightVal}
 	case "==":
-		fmt.Printf("%t", leftVal == rightVal)
 		return nativeBoolToBooleanObject(leftVal == rightVal)
 	case "!=":
 		return nativeBoolToBooleanObject(leftVal != rightVal)
@@ -366,4 +368,31 @@ func evalArrayIndexExpression(array, index object.Object) object.Object {
 	}
 
 	return arrayObject.Elements[idx]
+}
+
+func evalHashLiteral(node *ast.HashLiteral, env *object.Environment) object.Object {
+	pairs := make(map[object.HashKey]object.HashPair)
+
+	for keyNode, valueNode := range node.Pairs {
+
+		key := Eval(keyNode, env)
+		if isError(key) {
+			return key
+		}
+
+		hashKey, ok := key.(object.Hashable)
+		if !ok {
+			return newError("unusable as hash key: %s", key.Type())
+		}
+
+		value := Eval(valueNode, env)
+		if isError(value) {
+			return value
+		}
+
+		hashed := hashKey.HashKey()
+		pairs[hashed] = object.HashPair{Key: key, Value: value}
+	}
+
+	return &object.Hash{Pairs: pairs}
 }
