92f61a1be3ddf7935977d6257e0f339c44e990a3
Mizuho Mac <mizu.2340@gmail.com>
2020-03-07 17:26:08 +0900

    A.4.1.1 modify ast

---
+++ ast/modify.go
@@ -0,0 +1,18 @@
+package ast
+
+// ModifyFunc ...
+type ModifyFunc func(Node) Node
+
+// Modify ...
+func Modify(node Node, modifier ModifyFunc) Node {
+	switch node := node.(type) {
+	case *Program:
+		for i, statement := range node.Statements {
+			node.Statements[i], _ = Modify(statement, modifier).(Statement)
+		}
+	case *ExpressionStatement:
+		node.Expression, _ = Modify(node.Expression, modifier).(Expression)
+	}
+
+	return modifier(node)
+}
